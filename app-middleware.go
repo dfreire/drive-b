3f9725beb073b73d2f472805f89260899f3cba53
dfreire <dario.freire@gmail.com>
2016-04-11 22:08:46 +0100

    update

--- app-middleware.go
+++ app-middleware.go
@@ -1,17 +1,33 @@
 package driveb
 
 import (
+	"log"
+
 	"github.com/labstack/echo"
-	"github.com/mgutz/logxi/v1"
+	"github.com/labstack/echo/engine/standard"
 )
 
-func CreateAppMiddleware(authorizedURL string) echo.HandlerFunc {
-	return func(c *echo.Context) error {
+type appMiddleware struct{}
 
-		log.Info("UserAgent", c.Request().UserAgent())
-		log.Info("RemoteAddr", c.Request().RemoteAddr)
-		log.Info("RemoteAddr", c.Request().Header.Get("X-Forwarded-For"))
+func AppMiddleware(authorizedHosts []string) echo.MiddlewareFunc {
+	hostMap := make(map[string]bool)
+	for _, host := range authorizedHosts {
+		hostMap[host] = true
+	}
 
-		return nil
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+
+			host1 := c.Request().Host()
+			host2 := c.Request().(*standard.Request).Request.URL.Host
+
+			log.Println("Host from request:", host1)
+			log.Println("Host from url:", host2)
+
+			// log.Println("RemoteAddr:", c.Request().RemoteAddress())
+			// log.Println("X-Forwarded-For:", c.Request().Header().Get("X-Forwarded-For"))
+
+			return next(c)
+		}
 	}
 }
